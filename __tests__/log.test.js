d2111489d29c5986885572f4e7da4c0856d6dbce
dl-watson <dlarkinwatson@gmail.com>
2020-12-15 17:40:56 -0800

    tests passing

--- __tests__/log.test.js
+++ __tests__/log.test.js
@@ -11,7 +11,7 @@ describe("log-lab routes", () => {
   });
 
   afterAll(() => {
-    // return pool.end();
+    return pool.end();
   });
 
   it("creates a log", async () => {
@@ -44,104 +44,12 @@ describe("log-lab routes", () => {
       rating: 0,
     });
   });
-});
 
-it("gets all logs", async () => {
-  const recipe = await request(app)
-    .post("/api/v1/recipes")
-    .send({
-      name: "cookies",
-      directions: [
-        "preheat oven to 375",
-        "mix ingredients",
-        "put dough on cookie sheet",
-        "bake for 10 minutes",
-      ],
-    });
-
-  const logs = await Promise.all(
-    [
-      {
-        recipeId: recipe.body.id,
-        dateOfEvent: "January 16th, 2020",
-        notes: "These cookies were terrible.",
-        rating: 0,
-      },
-      {
-        recipeId: recipe.body.id,
-        dateOfEvent: "January 16th, 2020",
-        notes: "These cookies were terrible.",
-        rating: 0,
-      },
-      {
-        recipeId: recipe.body.id,
-        dateOfEvent: "January 16th, 2020",
-        notes: "These cookies were terrible.",
-        rating: 0,
-      },
-    ].map((log) => Log.insert(log))
-  );
-
-  return request(app)
-    .get("/api/v1/logs")
-    .then((res) => {
-      logs.forEach((log) => {
-        expect(res.body).toContainEqual(log);
-      });
-    });
-});
-
-it.only("gets a log by id", async () => {
-  const recipe = await request(app)
-    .post("/api/v1/recipes")
-    .send({
-      name: "cookies",
-      directions: [
-        "preheat oven to 375",
-        "mix ingredients",
-        "put dough on cookie sheet",
-        "bake for 10 minutes",
-      ],
-    });
-
-  const log = await Log.insert({
-    recipeId: recipe.id,
-    dateOfEvent: "January 16th, 2020",
-    notes: "These cookies were terrible.",
-    rating: 0,
-  });
-
-  return request(app)
-    .get(`/api/v1/logs/${log.id}`)
-    .then((res) => expect(res.body).toEqual(log));
-});
-
-it("updates a log by id", async () => {
-  const log = await log.insert({
-    name: "cookies",
-    directions: [
-      "preheat oven to 375",
-      "mix ingredients",
-      "put dough on cookie sheet",
-      "bake for 10 minutes",
-    ],
-  });
-
-  return request(app)
-    .put(`/api/v1/logs/${log.id}`)
-    .send({
-      name: "good cookies",
-      directions: [
-        "preheat oven to 375",
-        "mix ingredients",
-        "put dough on cookie sheet",
-        "bake for 10 minutes",
-      ],
-    })
-    .then((res) => {
-      expect(res.body).toEqual({
-        id: expect.any(String),
-        name: "good cookies",
+  it("gets all logs", async () => {
+    const recipe = await request(app)
+      .post("/api/v1/recipes")
+      .send({
+        name: "cookies",
         directions: [
           "preheat oven to 375",
           "mix ingredients",
@@ -149,13 +57,127 @@ it("updates a log by id", async () => {
           "bake for 10 minutes",
         ],
       });
+
+    const logs = await Promise.all(
+      [
+        {
+          recipeId: recipe.body.id,
+          dateOfEvent: "January 16th, 2020",
+          notes: "These cookies were terrible.",
+          rating: 0,
+        },
+        {
+          recipeId: recipe.body.id,
+          dateOfEvent: "January 16th, 2020",
+          notes: "These cookies were terrible.",
+          rating: 0,
+        },
+        {
+          recipeId: recipe.body.id,
+          dateOfEvent: "January 16th, 2020",
+          notes: "These cookies were terrible.",
+          rating: 0,
+        },
+      ].map((log) => Log.insert(log))
+    );
+
+    return request(app)
+      .get("/api/v1/logs")
+      .then((res) => {
+        logs.forEach((log) => {
+          expect(res.body).toContainEqual(log);
+        });
+      });
+  });
+
+  it("gets a log by id", async () => {
+    const recipe = await request(app)
+      .post("/api/v1/recipes")
+      .send({
+        name: "cookies",
+        directions: [
+          "preheat oven to 375",
+          "mix ingredients",
+          "put dough on cookie sheet",
+          "bake for 10 minutes",
+        ],
+      });
+
+    const log = await Log.insert({
+      recipeId: recipe.id,
+      dateOfEvent: "January 16th, 2020",
+      notes: "These cookies were terrible.",
+      rating: 0,
     });
-});
 
-it("updates a log by id", async () => {
-  const log = await log.insert({ name: "cookies", directions: [] });
+    return request(app)
+      .get(`/api/v1/logs/${log.id}`)
+      .then((res) => expect(res.body).toEqual(log));
+  });
 
-  return request(app)
-    .delete(`/api/v1/logs/${log.id}`)
-    .then((res) => expect(res.body).toEqual(log));
+  it("updates a log by id", async () => {
+    const recipe = await request(app)
+      .post("/api/v1/recipes")
+      .send({
+        name: "cookies",
+        directions: [
+          "preheat oven to 375",
+          "mix ingredients",
+          "put dough on cookie sheet",
+          "bake for 10 minutes",
+        ],
+      });
+
+    const log = await request(app).post("/api/v1/logs").send({
+      recipeId: recipe.body.id,
+      dateOfEvent: "January 16th, 2020",
+      notes: "These cookies were terrible.",
+      rating: 0,
+    });
+
+    return request(app)
+      .put(`/api/v1/logs/${log.body.id}`)
+      .send({
+        recipeId: recipe.body.id,
+        dateOfEvent: "February 27th, 2020",
+        notes: "JK, these cookies were great!",
+        rating: 5,
+      })
+      .then((res) =>
+        expect(res.body).toEqual(
+          expect.objectContaining({
+            id: expect.any(String),
+            recipeId: recipe.body.id,
+            dateOfEvent: "February 27th, 2020",
+            notes: "JK, these cookies were great!",
+            rating: 5,
+          })
+        )
+      );
+  });
+
+  it("deletes a log by id", async () => {
+    const recipe = await request(app)
+      .post("/api/v1/recipes")
+      .send({
+        name: "cookies",
+        directions: [
+          "preheat oven to 375",
+          "mix ingredients",
+          "put dough on cookie sheet",
+          "bake for 10 minutes",
+        ],
+      });
+
+    const log = await request(app).post("/api/v1/logs").send({
+      recipeId: recipe.body.id,
+      dateOfEvent: "January 16th, 2020",
+      notes: "These cookies were terrible.",
+      rating: 0,
+    });
+
+    return request(app)
+      .delete(`/api/v1/logs/${log.body.id}`)
+      .then((res) => expect(res.body).toEqual(log.body));
+  });
 });
